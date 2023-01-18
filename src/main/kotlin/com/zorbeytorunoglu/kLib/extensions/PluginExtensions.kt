9f268e36902fbdb36f70abb92481daa3a73713a5
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-18 18:02:14 +0300

    Added fast log functions

--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/PluginExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/PluginExtensions.kt
@@ -1,8 +1,14 @@
 package com.zorbeytorunoglu.kLib.extensions
 
+import com.zorbeytorunoglu.kLib.MCPlugin
 import org.bukkit.event.Listener
-import org.bukkit.plugin.Plugin
 
-fun Plugin.registerEvents(vararg listeners: Listener) {
+fun MCPlugin.registerEvents(vararg listeners: Listener) {
     listeners.forEach { server.pluginManager.registerEvents(it, this) }
 }
+
+fun MCPlugin.info(log: String) = this.logger.info(log)
+
+fun MCPlugin.severe(log: String) = this.logger.severe(log)
+
+fun MCPlugin.warning(log: String) = this.logger.warning(log)
