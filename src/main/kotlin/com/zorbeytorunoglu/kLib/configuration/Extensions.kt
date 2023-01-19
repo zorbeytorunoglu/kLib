084c87b51eb6a1aab82025a13e8f78ff4c26a615
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-19 05:59:59 +0300

    Changed to MCPlugin instead of JavaPlugin

--- src/main/kotlin/com/zorbeytorunoglu/kLib/configuration/Extensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/configuration/Extensions.kt
@@ -1,11 +1,11 @@
 package com.zorbeytorunoglu.kLib.configuration
 
+import com.zorbeytorunoglu.kLib.MCPlugin
 import org.bukkit.configuration.file.YamlConfiguration
-import org.bukkit.plugin.java.JavaPlugin
 
-fun JavaPlugin.createYamlResource(fileName: String): Resource = Resource(this, fileName)
+fun MCPlugin.createYamlResource(fileName: String): Resource = Resource(this, fileName)
 
-fun JavaPlugin.createLoadYamlResource(fileName: String): YamlConfiguration =
+fun MCPlugin.createLoadYamlResource(fileName: String): YamlConfiguration =
     YamlConfiguration.loadConfiguration(Resource(this, fileName).file)
 
-fun JavaPlugin.createFileWithPath(path: String, fileName: String): Resource = Resource(path, fileName)
+fun MCPlugin.createFileWithPath(path: String, fileName: String): Resource = Resource(path, fileName)
