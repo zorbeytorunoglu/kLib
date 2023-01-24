3d8ac33eee8550308cc8e55759d2d8a78bcf96b2
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-24 05:03:31 +0300

    Added suspendFunctionWithResult

--- src/main/kotlin/com/zorbeytorunoglu/kLib/task/Extensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/task/Extensions.kt
@@ -1,28 +1,52 @@
 package com.zorbeytorunoglu.kLib.task
 
 import com.zorbeytorunoglu.kLib.MCPlugin
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 suspend fun MCPlugin.suspendFunctionAsync(function: () -> Unit) {
-
     Scopes.supervisorScope.launch(MCDispatcher(this, async = true)) {
         function()
     }.join()
-
 }
 
 suspend fun MCPlugin.suspendFunctionSync(function: () -> Unit) {
-
     Scopes.supervisorScope.launch(MCDispatcher(this, async = false)) {
         function()
     }.join()
-
 }
 
 suspend fun MCPlugin.suspendFunction(function: () -> Unit, async: Boolean) {
-
     Scopes.supervisorScope.launch(MCDispatcher(this, async = async)) {
         function()
     }.join()
+}
+
+suspend fun <T> MCPlugin.suspendFunctionSyncWithResult(function: () -> T): T {
+
+    return Scopes.supervisorScope.async(MCDispatcher(this,async = false)) {
+
+        function()
+
+    }.await()
+
+}
+
+suspend fun <T> MCPlugin.suspendFunctionAsyncWithResult(function: () -> T): T {
+
+    return Scopes.supervisorScope.async(MCDispatcher(this,async = true)) {
+
+        function()
+
+    }.await()
+
+}
+
+suspend fun <T> MCPlugin.suspendFunctionWithResult(function: () -> T, async: Boolean): T {
+
+    return Scopes.supervisorScope.async(MCDispatcher(this,async)) {
+
+        function()
+
+    }.await()
 
 }
