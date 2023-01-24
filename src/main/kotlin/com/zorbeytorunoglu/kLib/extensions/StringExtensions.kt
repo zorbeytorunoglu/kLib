4241cb39c5c8d14be3a47bf2aeff494439be9abb
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-24 05:04:58 +0300

    Renamed "containsDigit" method to "containsNumbers"

--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/StringExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/StringExtensions.kt
@@ -71,14 +71,14 @@ fun String.isEmailValid(): Boolean {
 val String.containsLatinLetter: Boolean
     get() = matches(Regex(".*[A-Za-z].*"))
 
-val String.containsDigit: Boolean
+val String.containsNumbers: Boolean
     get() = matches(Regex(".*[0-9].*"))
 
 val String.isAlphanumeric: Boolean
     get() = matches(Regex("[A-Za-z0-9]*"))
 
 val String.hasLettersAndDigits: Boolean
-    get() = containsLatinLetter && containsDigit
+    get() = containsLatinLetter && containsNumbers
 
 val String.isIntegerNumber: Boolean
     get() = toIntOrNull() != null
