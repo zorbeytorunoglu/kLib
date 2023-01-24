5b6e3cf57b8cca3c39468c39e94a4ca9d610a3e9
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-24 05:05:24 +0300

    Added chunkToString, chunkFromString methods

--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/WorldExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/WorldExtensions.kt
@@ -1,6 +1,22 @@
 package com.zorbeytorunoglu.kLib.extensions
 
+import org.bukkit.Bukkit
+import org.bukkit.Chunk
 import org.bukkit.World
 
 fun World.removeEntities() = this.entities.forEach { it.remove() }
 
+fun Chunk.toLegibleString(): String = this.world.name+","+this.x+","+this.z
+
+fun Chunk.fromString(string: String): Chunk? {
+
+    val varargs = string.split(",")
+
+    return try {
+        val world = Bukkit.getWorld(varargs[0])
+        world.getChunkAt(varargs[1].toInt(),varargs[2].toInt())
+    } catch (e: Exception) {
+        null
+    }
+
+}
