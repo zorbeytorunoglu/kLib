b61bf019a00c451e4b244f47ee899c6b7131f5ab
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-28 02:20:21 +0300

    Added JavaDoc

--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/IntegerExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/IntegerExtensions.kt
@@ -1,3 +1,9 @@
 package com.zorbeytorunoglu.kLib.extensions
 
+/**
+ * Returns a random integer between specified integers.
+ * @param from From
+ * @param to To
+ * @return A random integer between two parameters.
+ */
 fun Int.random(from: Int, to:Int): Int = (from..to).random()
--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/PlayerExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/PlayerExtensions.kt
@@ -1,6 +1,7 @@
 package com.zorbeytorunoglu.kLib.extensions
 
 import com.zorbeytorunoglu.kLib.MCPlugin
+import com.zorbeytorunoglu.kLib.cuboid.Cuboid
 import com.zorbeytorunoglu.kLib.task.MCDispatcher
 import com.zorbeytorunoglu.kLib.task.Scopes
 import kotlinx.coroutines.launch
@@ -12,50 +13,86 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.util.*
 
-
+/**
+ * Checks if the player is in the cuboid region that is formed by the two given locations.
+ * @param playerLocation Location of the player.
+ * @param location1 First location
+ * @param location2 Second location
+ * @return True if the player is in the cuboid, false if not.
+ */
 fun Player.isInside(playerLocation: Location, location1: Location, location2: Location): Boolean {
 
-    val x: Int = playerLocation.blockX
-    val y: Int = playerLocation.blockY
-    val z: Int = playerLocation.blockZ
-    val x1: Int = location1.blockX.coerceAtMost(location2.blockX)
-    val y1: Int = location1.blockY.coerceAtMost(location2.blockY)
-    val z1: Int = location1.blockZ.coerceAtMost(location2.blockZ)
-    val x2: Int = location1.blockX.coerceAtLeast(location2.blockX)
-    val y2: Int = location1.blockY.coerceAtLeast(location2.blockY)
-    val z2: Int = location1.blockZ.coerceAtLeast(location2.blockZ)
+    val cuboid = Cuboid(location1, location2)
 
-    return x in x1..x2 && y >= y1 && y <= y2 && z >= z1 && z <= z2
+    return cuboid.isIn(playerLocation)
 
 }
 
+/**
+ * Checks if the player's inventory is full.
+ * @return Boolean: True if there is no empty slot, false if there is.
+ */
 fun Player.isInventoryFull(): Boolean {
     return this.inventory.firstEmpty() == -1;
 }
 
+/**
+ * Clears all the armor player wears.
+ */
 fun Player.clearArmor() {
     player.inventory.armorContents = arrayOf<ItemStack?>(null, null, null, null)
 }
 
+/**
+ * Clears both all the armors and inventory together.
+ */
 fun Player.clearAllInventory() {
     inventory.clear()
     clearArmor()
 }
 
+/**
+ * Gets the item in hand if it is not null and the type is not AIR.
+ * @return ItemStack
+ */
 val Player.hasItemInHand get() = itemInHand != null && itemInHand.type != Material.AIR
 
+/**
+ * Plays sound in player's location.
+ * @param sound Sound enum
+ * @param volume Volume float
+ * @param pitch Pitch float
+ */
 fun Player.playSound(sound: Sound, volume: Float, pitch: Float) = playSound(location, sound, volume, pitch)
 
+/**
+ * Plays an effect in player's location.
+ * @param effect Effect
+ * @param data Effect data
+ */
 fun <T> Player.playEffect(effect: Effect, data: T? = null) = playEffect(player.location, effect, data)
 
+/**
+ * Resets the walk speed of the player.
+ */
 fun Player.resetWalkSpeed() {
     player.walkSpeed = 0.2f
 }
 
+/**
+ * Resets the fly speed of the player.
+ */
 fun Player.resetFlySpeed() {
     player.flySpeed = 0.1f
 }
 
+/**
+ * Teleports the player to the location without causing halt.
+ * It actually teleports them synchronized since you can not access the Bukkit API
+ * async, yet it is named this way since all the Spigot developers are used to this term due to other libraries/APIs.
+ * @param mcPlugin MCPlugin
+ * @param location Location
+ */
 suspend fun Player.teleportAsync(mcPlugin: MCPlugin, location: Location) {
     Scopes.supervisorScope.launch(MCDispatcher(mcPlugin, async = false)) {
 
@@ -64,6 +101,13 @@ suspend fun Player.teleportAsync(mcPlugin: MCPlugin, location: Location) {
     }.join()
 }
 
+/**
+ * Teleports the player to the other player without causing halt.
+ * It actually teleports them synchronized since you can not access the Bukkit API
+ * async, yet it is named this way since all the Spigot developers are used to this term due to other libraries/APIs.
+ * @param mcPlugin MCPlugin
+ * @param targetPlayer Player
+ */
 suspend fun Player.teleportAsync(mcPlugin: MCPlugin, targetPlayer: Player) {
     Scopes.supervisorScope.launch(MCDispatcher(mcPlugin, async = false)) {
 
@@ -72,6 +116,13 @@ suspend fun Player.teleportAsync(mcPlugin: MCPlugin, targetPlayer: Player) {
     }.join()
 }
 
+/**
+ * Teleports the entity to the location without causing halt.
+ * It actually teleports them synchronized since you can not access the Bukkit API
+ * async, yet it is named this way since all the Spigot developers are used to this term due to other libraries/APIs.
+ * @param mcPlugin MCPlugin
+ * @param location Location
+ */
 suspend fun Entity.teleportAsync(mcPlugin: MCPlugin, location: Location) {
     Scopes.supervisorScope.launch(MCDispatcher(mcPlugin, async = false)) {
 
@@ -80,6 +131,13 @@ suspend fun Entity.teleportAsync(mcPlugin: MCPlugin, location: Location) {
     }.join()
 }
 
+/**
+ * Teleports the entity to the other entity without causing halt.
+ * It actually teleports them synchronized since you can not access the Bukkit API
+ * async, yet it is named this way since all the Spigot developers are used to this term due to other libraries/APIs.
+ * @param mcPlugin MCPlugin
+ * @param entity Entity
+ */
 suspend fun Entity.teleportAsync(mcPlugin: MCPlugin, entity: Entity) {
 
     Scopes.supervisorScope.launch(MCDispatcher(mcPlugin, async = false)) {
@@ -90,26 +148,50 @@ suspend fun Entity.teleportAsync(mcPlugin: MCPlugin, entity: Entity) {
 
 }
 
+/**
+ * Bans the player with the given reason.
+ * @param reason Reason string
+ */
 fun Player.ban(reason: String) {
     this.kickPlayer(reason)
     Bukkit.getBanList(BanList.Type.NAME).addBan(this.name, reason, null, null)
 }
 
+/**
+ * Bans the player with the given reason and source.
+ * @param reason Reason string
+ * @param source Source string
+ */
 fun Player.ban(reason: String, source: String) {
     this.kickPlayer(reason)
     Bukkit.getBanList(BanList.Type.NAME).addBan(this.name, reason, null, source)
 }
 
+/**
+ * Bans the player temporarily until the expiration date with the given reason and given source.
+ * @param reason Reason string
+ * @param source Source string
+ * @param expiration Expiration date
+ */
 fun Player.ban(reason: String, source: String, expiration: Date) {
     this.kickPlayer(reason)
     Bukkit.getBanList(BanList.Type.NAME).addBan(this.name, reason, expiration, source)
 }
 
-fun Player.ban(reason: String?, expiration: Date) {
+/**
+ * Bans the player temporarily until the expiration date with the given reason.
+ * @param reason Reason string
+ * @param expiration Expiration date
+ */
+fun Player.ban(reason: String, expiration: Date) {
     this.kickPlayer(reason)
     Bukkit.getBanList(BanList.Type.NAME).addBan(this.name, reason, expiration, null)
 }
 
+/**
+ * Bans the player's IP address with the given reason.
+ * @param reason Reason string
+ */
 fun Player.banIP(reason: String?) {
     this.kickPlayer(reason)
     if (this.address != null) {
@@ -117,6 +199,11 @@ fun Player.banIP(reason: String?) {
     }
 }
 
+/**
+ * Bans the player's IP address with the given reason and source.
+ * @param reason Reason string
+ * @param source Source string
+ */
 fun Player.banIP(reason: String?, source: String?) {
     this.kickPlayer(reason)
     if (this.address != null) {
@@ -124,28 +211,55 @@ fun Player.banIP(reason: String?, source: String?) {
     }
 }
 
-fun Player.banIP(reason: String?, source: String?, expiration: Date?) {
+/**
+ * Bans the player's IP address temporarily until the expiration date with the given reason and source.
+ * @param reason Reason string
+ * @param source Source string
+ * @param expiration Expiration date
+ */
+fun Player.banIP(reason: String, source: String, expiration: Date) {
     this.kickPlayer(reason)
     if (this.address != null) {
         Bukkit.getBanList(BanList.Type.IP).addBan(this.address.address.hostAddress, reason, expiration, source)
     }
 }
 
+/**
+ * Heals and feeds the player.
+ */
 fun Player.heal() {
     this.health = 20.0
     this.foodLevel = 20
 }
 
+/**
+ * Feeds the player.
+ */
 fun Player.feed() {
     this.foodLevel = 20
 }
 
+/**
+ * Sends a "String Collection" to the player as a message.
+ * @param messageList String Collection
+ */
 fun Player.sendMessage(messageList: Collection<String>) = messageList.forEach { this.sendMessage(it) }
 
+/**
+ * Sends a "String Collection" to the command sender as a message.
+ * @param messageList String Collection
+ */
 fun CommandSender.sendMessage(messageList: Collection<String>) = messageList.forEach { this.sendMessage(it) }
 
+/**
+ * Gets the block below the player.
+ * @return Block
+ */
 fun Player.getBlockBelow(): Block = this.location.subtract(0.0, 1.0, 0.0).block
 
+/**
+ * Kills the player.
+ */
 fun Player.kill() {
     player.health = 0.0
 }
