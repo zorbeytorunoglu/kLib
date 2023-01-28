eb9afa2539d567c8dcc8a04f06f1651c4c3b08a4
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-28 20:22:30 +0300

    Added JavaDoc

--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/WorldExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/WorldExtensions.kt
@@ -5,10 +5,22 @@ import org.bukkit.Chunk
 import org.bukkit.Sound
 import org.bukkit.World
 
+/**
+ * Removes all the entities in the world's list.
+ */
 fun World.removeEntities() = this.entities.forEach { it.remove() }
 
+/**
+ * Parses the chunk to string.
+ * @return String
+ */
 fun Chunk.toLegibleString(): String = this.world.name+","+this.x+","+this.z
 
+/**
+ * Gets a chunk from string.
+ * @param string String
+ * @return Chunk
+ */
 fun Chunk.fromString(string: String): Chunk? {
 
     val varargs = string.split(",")
@@ -22,6 +34,12 @@ fun Chunk.fromString(string: String): Chunk? {
 
 }
 
+/**
+ * Plays sound for player in the world.
+ * @param sound Sound
+ * @param volume Volume Float
+ * @param pitch Pitch Float
+ */
 fun World.playSound(sound: Sound, volume: Float, pitch: Float) {
     this.players.forEach {
         it.playSound(sound, volume, pitch)
