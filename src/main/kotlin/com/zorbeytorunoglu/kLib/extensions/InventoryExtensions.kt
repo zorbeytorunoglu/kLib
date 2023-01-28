34399962a33f117d826696cb57b646e2000ce0f8
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-29 02:40:39 +0300

    Added JavaDoc

--- src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/InventoryExtensions.kt
+++ src/main/kotlin/com/zorbeytorunoglu/kLib/extensions/InventoryExtensions.kt
@@ -6,6 +6,13 @@ import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 import org.bukkit.inventory.ItemStack
 
+/**
+ * Creates an Inventory.
+ * @param type InventoryType
+ * @param owner InventoryHolder
+ * @param title Title String
+ * @return Inventory
+ */
 fun Inventory(type: InventoryType, owner: InventoryHolder? = null, title: String? = null): Inventory {
     return if (title != null)
         Bukkit.getServer().createInventory(owner, type, title)
@@ -13,6 +20,13 @@ fun Inventory(type: InventoryType, owner: InventoryHolder? = null, title: String
         Bukkit.getServer().createInventory(owner, type)
 }
 
+/**
+ * Creates a limited sized Inventory.
+ * @param size Size, Int
+ * @param owner InventoryHolder
+ * @param title Title, String
+ * @return Inventory
+ */
 fun Inventory(size: Int, owner: InventoryHolder? = null, title: String? = null): Inventory {
     return if (title != null)
         Bukkit.getServer().createInventory(owner, size, title)
@@ -20,10 +34,21 @@ fun Inventory(size: Int, owner: InventoryHolder? = null, title: String? = null):
         Bukkit.getServer().createInventory(owner, size)
 }
 
+/**
+ * Checks if there is any available space for the given ItemStack.
+ * @param itemStack ItemStack
+ * @param amount Amount, Int
+ * @return Boolean
+ */
 fun Inventory.hasSpace(itemStack: ItemStack, amount: Int = itemStack.amount): Boolean {
     return getSpaceOf(itemStack) >= amount
 }
 
+/**
+ * Gets the number of the available space of the given ItemStack.
+ * @param itemStack ItemStack
+ * @return Int
+ */
 fun Inventory.getSpaceOf(itemStack: ItemStack): Int {
     return contents.filterNotNull().map {
         if (it.amount < it.maxStackSize && it.isSimilar(itemStack))
@@ -32,6 +57,13 @@ fun Inventory.getSpaceOf(itemStack: ItemStack): Int {
     }.count()
 }
 
+/**
+ * Clones the Inventory.
+ * @param cloneItemStacks Boolean
+ * @param owner InventoryHolder
+ * @param title Title
+ * @return Inventory
+ */
 fun Inventory.clone(cloneItemStacks: Boolean = true, owner: InventoryHolder? = holder, title: String? = null): Inventory {
     val inventory = if (type == InventoryType.CHEST)
         Inventory(size, owner, title)
