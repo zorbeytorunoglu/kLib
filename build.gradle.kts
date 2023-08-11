799a7e18705d194b32e0c96229dfb44a3c5b072e
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-08-12 00:03:24 +0300

    Updated Kotlin stdlib and coroutines version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.9.0"
     id("java")
     `maven-publish`
     id ("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "com.zorbeytorunoglu"
-version = "0.0.5"
+version = "0.0.6"
 
 repositories {
     mavenCentral()
@@ -19,8 +19,8 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.apache.maven:maven-artifact:3.8.7")
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 }
@@ -42,7 +42,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.zorbeytorunoglu"
             artifactId = "kLib"
-            version = "0.0.5"
+            version = "0.0.6"
 
             from(components["java"])
         }
